a6914d63ddb8a8269801d1ffc85afaf00f9b1d14
Gabriel Massaia de Oliveira <gabrielmassaia50@gmail.com>
2023-04-21 17:47:22 -0300

    up

---
+++ Lógica de Programação e Algoritmos/index03.py
@@ -0,0 +1,27 @@
+# a) Atribuir o valor inteiro '3' à variável 'a'
+# b) Atribuir o valor 4 à variável b
+# c) Atribuir à variável co valor da expressão
+a = 3
+b = 4
+c = ((a*a) + b *b)
+print(c) 
+
+#Strings
+s1 = 'ant'
+s2 = 'bat'
+s3 = 'cod'
+
+# # a)
+print(s1, s2, s3)
+
+# # b)
+print((s1 + ' ') *10)
+
+# # c)
+print((s1 + ' ') + (s2 + ' ')*2 + (s3 + ' ')*3)
+
+# # d)
+print(((s1 + ' ') + (s2 +' ')) *7)
+
+# e)
+print((s2 + s2 + s3 + ' ')*5)
