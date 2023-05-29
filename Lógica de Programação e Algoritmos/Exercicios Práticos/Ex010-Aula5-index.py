50f0cf6c6f06150bbbe13876db7ec87b2b553074
Gabriel Massaia <gabrielmassaia50@gmail.com>
2023-05-28 21:24:05 -0300

    functions

---
+++ Lógica de Programação e Algoritmos/Exercicios Práticos/Ex010-Aula5-index.py
@@ -0,0 +1,19 @@
+#Calculando fatorial com for
+
+def valida_int(pergunta, min, max):
+    x = int(input(pergunta))
+    while ((x < min) or (x > max)):
+        x = int(input(pergunta))
+    return x
+
+def fatorial(num):
+    fat = 1
+    if num == 0:
+        return fat
+    for i in range(1, num+1, 1):
+        fat *= i # fat = fat * i
+    return fat
+
+x = valida_int("Digite um valor para calcular a fatorial: ", 0, 99999)
+print("{}! = {}".format(x, fatorial(x)))
+
